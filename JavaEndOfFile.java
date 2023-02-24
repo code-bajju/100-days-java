ef91434f4b6fa43707f871dedb38f6185d3f466f
code-bajju <11212766@mmec.mmdu.org>
2023-02-24 20:35:35 +0530

    Java end of file

---
+++ JavaEndOfFile.java
@@ -0,0 +1,18 @@
+    import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+      int test=1 ;
+     try( Scanner sc = new Scanner(System.in)){
+         while (sc.hasNext()) {
+             System.out.println(test+" "+ sc.nextLine());
+            ++test;
+         }
+     }
+    }
+}
